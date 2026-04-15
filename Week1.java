44539ae4b9bb9caf235286fca2ac1ea13ae68262
kapil212a <amangupta74086@gmail.com>
2026-04-15 19:47:33 +0530

    change

--- Week1.java
+++ Week1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Week1 {
     //// Minimum Distnace from target////
     
@@ -13,10 +16,32 @@ public class Week1 {
         return minDistance;
     }
 
+    //// Longest Substring Without Repeating Characters////
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map <Character , Integer> map = new HashMap<>();
+        int left = 0 ,length = 0;
+
+        for(int i = 0; i<s.length() ; i++){
+            char ch = s.charAt(i);
+
+            if(map.containsKey(ch)){
+                left = Math.max(left , map.get(ch) +1);
+            }
+
+            map.put(ch , i);
+            length = Math.max(length , i - left +1);
+        }
+        return length;
+    }
+
     public static void main(String[] args) {
         int nums [] = {1,2,3,4,5};
         int target = 5;
         int start = 3;
         System.out.println(getMinDistance(nums , target , start));
+
+        String s = "abccbabac";
+        System.out.println(lengthOfLongestSubstring(s));
     }
 }
