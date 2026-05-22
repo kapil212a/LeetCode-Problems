c954ec65c66d126f4243fd4e429bb9605d2b8409
kapil212a <amangupta74086@gmail.com>
2026-05-23 00:19:49 +0530

    chnge

--- Week2.java
+++ Week2.java
@@ -55,7 +55,23 @@ public class Week2 {
         return total;
     }
 
+    //// Longest Common Prefix ////////
     
+    public static String longestCommonPrefix(String[] strs) {
+        String first = strs[0];
+
+        for(int i = 0; i<first.length(); i++){
+            char ch = first.charAt(i);
+
+            for(int j = 1; j<strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+                    return first.substring(0,i);
+                }
+            }
+        }
+        return first;
+    }
+
 
     public static void main(String[] args) {
         int num = 12357;
@@ -63,6 +79,9 @@ public class Week2 {
 
         String s = "MCMXCIV";
         System.out.println(romanToInt(s));
+
+        String [] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
     }
 }
 
