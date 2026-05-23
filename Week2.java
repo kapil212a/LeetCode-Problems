f2b5cb405077f31a9d09b1b01e2e396410be66b4
kapil212a <amangupta74086@gmail.com>
2026-05-24 02:01:13 +0530

    add

--- Week2.java
+++ Week2.java
@@ -117,7 +117,36 @@ public class Week2 {
         }
         return list;
     }
+//// 3 sum closest////
 
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i<nums.length-2; i++){
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(Math.abs(target - sum) < Math.abs(target - res)){
+                    res = sum;
+                }
+                if(sum == target){
+                    return sum;
+                }
+                else if (sum < target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
 
 
     public static void main(String[] args) {
@@ -132,6 +161,10 @@ public class Week2 {
 
         int arr1[] = {-1,0,1,2,-1,-4};
         System.out.println(threeSum(arr1));
+
+        int arr2[] = {-1,2,1,-4};
+        int target = 1;
+        System.out.println(threeSumClosest(arr2, target));
     }
 }
 
