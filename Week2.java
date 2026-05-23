1c81c7213706b68793c555d77ddd311f2419c27c
kapil212a <amangupta74086@gmail.com>
2026-05-24 01:26:12 +0530

    ad

--- Week2.java
+++ Week2.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Week2 {
     ////// Integer To Roman//////
     
@@ -72,6 +76,49 @@ public class Week2 {
         return first;
     }
 
+    /// 3 SUM////
+    
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length -1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+
+                    while(left < right && nums[left] == nums[left + 1]){
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+                
+            }
+            
+        }
+        return list;
+    }
+
+
 
     public static void main(String[] args) {
         int num = 12357;
@@ -82,6 +129,9 @@ public class Week2 {
 
         String [] strs = {"flower","flow","flight"};
         System.out.println(longestCommonPrefix(strs));
+
+        int arr1[] = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(arr1));
     }
 }
 
