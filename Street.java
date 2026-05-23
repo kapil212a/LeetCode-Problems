36755884d616927c92e9225203842a569e0c4911
kapil212a <amangupta74086@gmail.com>
2026-05-24 00:05:59 +0530

    ch

--- Street.java
+++ Street.java
@@ -32,9 +32,34 @@ public class Street {
         return -1;
     }
 
+    /// Maximum Sum Of rotating Array By Multiplying With Index Number////
+    
+    public static int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        int total = 0;
+        for(int i = 0; i<nums.length; i++){
+            res += nums[i];
+            total += nums[i] * i;
+        }
+        int current = total;
+        int max = total;
+
+        for(int i = 1; i<n; i++){
+            current = current + res - n * nums[n-i];
+
+            max = Math.max(current, max);
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
         int num [] = {4,5,6,7,0,1,2};
         System.out.println(search(num, 0));
+
+        int arr[] = {4,3,2,6};
+        System.out.println(maxRotateFunction(arr));
     }
 }
 
