2aa03a763d6fe488a46ae5fabcdecd34d884ac8e
kapil212a <amangupta74086@gmail.com>
2026-05-25 02:01:50 +0530

    change

--- Week2.java
+++ Week2.java
@@ -179,6 +179,54 @@ public class Week2 {
         }
     }
 
+///// 4SUM ////// 
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for(int i = 0; i<n - 3; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            for(int j = i+1; j<n-2;j++){
+                if(j > i+1 && nums[j] == nums[j -1]){
+                    continue;
+                }
+
+                int left = j+1;
+                int right = n-1;
+
+                while(left < right){
+                    long sum =(long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if(sum == target){
+
+                        res.add(Arrays.asList(nums[i] , nums[j] ,nums[left], nums[right]));
+
+                        while(left < right && nums[left] == nums[left +1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right -1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                    else if(sum < target){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
 
 
     public static void main(String[] args) {
@@ -200,6 +248,10 @@ public class Week2 {
 
         String s1 = "23";
         System.out.println(letterCombinations(s1));
+
+        int arr3[] = {1,0,-1,0,-2,2};
+        int target1 = 0;
+        System.out.println(fourSum(arr3, target1));
     }
 }
 
