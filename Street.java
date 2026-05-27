d643fc119909a058cff87b706054edd3beef21b7
kapil212a <amangupta74086@gmail.com>
2026-05-28 00:10:46 +0530

    ch

--- Street.java
+++ Street.java
@@ -170,6 +170,37 @@ public class Street {
 
     ////// Count the number of Spacial Character II \\\\\\\\\
 
+    public static int numberOfSpecialChars2(String word) {
+        int [] lower = new int [26];
+        int [] upper = new int[26];
+
+        Arrays.fill(lower , -1);
+        Arrays.fill(upper , -1);
+
+        for(int i = 0; i< word.length(); i++){
+
+            char ch = word.charAt(i);
+
+            if(Character.isLowerCase(ch)){
+                lower[ch - 'a'] = i;
+            }
+            else{
+                int idx = ch - 'A';
+
+                if(upper[idx] == -1){
+                    upper[idx] = i;
+                }
+            }
+        }
+        int count = 0;
+
+        for(int i = 0; i < 26; i++){
+            if(lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]){
+                count++;
+            }
+        }
+        return count;
+    }
     
 
 
