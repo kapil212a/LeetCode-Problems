01a9280a26b067ffbc5ad56f203072dc96b8632b
kapil212a <amangupta74086@gmail.com>
2026-06-02 23:46:03 +0530

    ch

--- Week1.java
+++ Week1.java
@@ -237,6 +237,31 @@ public class Week1 {
         return maxwater;
     }
 
+    ////////// TRAPING RAINWATER \\\\\\\\\\
+
+    public static int trap(int[] height) {
+        int n = height.length;
+
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        left[0] = height[0];
+        for(int i = 1; i< n; i++){
+            left[i] = Math.max(left[i -1], height[i]);
+        }
+
+        right[n-1] = height[n -1];
+        for(int i = n-2; i >= 0; i--){
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
+
+        int totalWater = 0;
+        for(int i = 0; i < n; i++){
+            totalWater += Math.min(left[i] , right[i]) - height[i];
+        }
+        return totalWater;
+    }
+
 
     public static void main(String[] args) {
         int nums [] = {1,2,3,4,5};
@@ -269,6 +294,9 @@ public class Week1 {
 
         int arr[] = {1,8,6,2,5,4,8,3,7};
         System.out.println(maxArea(arr));
+
+        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height));
 
     }
 }
