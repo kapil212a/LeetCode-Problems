8b3b7686746eb60b5f048aea6ea4b90abdcdb7f6
kapil212a <amangupta74086@gmail.com>
2026-06-04 00:02:21 +0530

    add

--- Street.java
+++ Street.java
@@ -260,6 +260,43 @@ public class Street {
         return ans;
     }
 
+    ////////PRODUCT OF TWO STRING INTEGER \\\\\\\\
+
+    public static String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
+        }
+
+        int n = num1.length();
+        int m = num2.length();
+
+        int res[] = new int[n + m];
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + res[pos2];
+
+                res[pos2] = sum % 10;
+                res[pos1] += sum / 10;
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+
+        for(int num : res){
+            if(!(ans.length() == 0 && num == 0)){
+                ans.append(num);
+            }
+        }
+        return ans.toString();
+    }
 
     public static void main(String[] args) {
         int num [] = {4,5,6,7,0,1,2};
@@ -294,6 +331,10 @@ public class Street {
         int[] waterStartTime = {6},  waterDuration = {3};
         System.out.println(earliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration));
 
+        String num1 = "123";
+        String num2 = "456";
+
+        System.out.println(multiply(num1, num2));
 
     }
 }
