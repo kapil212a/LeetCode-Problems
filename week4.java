ae6a3ae6fd085524ab0438d6741f6d749faf4c22
kapil212a <amangupta74086@gmail.com>
2026-06-05 23:10:36 +0530

    add

--- week4.java
+++ week4.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class week4 {
     /////////// EARLIER FINISH TIME OF WATER AND LAND \\\\\\\\\
     public static int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
@@ -71,6 +74,36 @@ public class week4 {
         return jump;
     }
 
+    ////////// PERMUTATION \\\\\\\\\\
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        solve(0 , nums,ans);
+
+        return ans;
+    }
+    private static void solve(int idx , int [] nums , List<List<Integer>> ans){
+        if(idx == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int num : nums){
+                temp.add(num);
+            }
+            ans.add(temp);
+            return;
+        }
+        for(int i = idx; i < nums.length; i++){
+            swap(nums , idx, i);
+            solve(idx + 1 , nums , ans);
+            swap(nums , idx, i);
+        }
+    }
+    private static void swap(int nums[] , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+     
     public static void main(String[] args) {
        int landStartTime[] = {2,8}, landDuration[] = {4,1};
        int waterStartTime [] = {6}, waterDuration[] = {3};
@@ -81,6 +114,9 @@ public class week4 {
 
         int nums1[] = {2,3,1,1,4};
        System.out.println(jump(nums1));
+
+       int nums2[] = {1,2,3};
+       System.out.println(permute(nums2));
 
     }
 }
