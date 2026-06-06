325f93cff2a6fd8b3e246bfc825c77042c109467
kapil212a <amangupta74086@gmail.com>
2026-06-06 22:14:23 +0530

    chanage

--- Street1.java
+++ Street1.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Street1 {
     ///////// PRINT ARRAY \\\\\\\\
     
@@ -30,9 +34,47 @@ public class Street1 {
         }
         return ans;
     }
+
+    /////// PERMUTATION II \\\\\\\\\\
+    
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        boolean used[] = new boolean [n];
+
+        backtrack(nums, used, new ArrayList(), ans);
+
+        return ans;
+    }
+    private static void backtrack(int [] nums, boolean[] used, List<Integer> curr, List<List<Integer>> ans){
+
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(used[i]){
+                continue;
+            }
+            if(i > 0 && nums[i] == nums[i - 1] && !used[i - 1]){
+                continue;
+            }
+            used[i] = true;
+            curr.add(nums[i]);
+
+            backtrack(nums, used, curr, ans);
+            curr.remove(curr.size() - 1);
+            used[i] = false;
+        }
+    }
+
     public static void main(String[] args) {
         int nums[] = {10,4,8,3};
         printArray(leftRightDifference(nums));
+
+       
 
     }
 }
