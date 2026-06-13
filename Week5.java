6d2f5eeb00f45465727306c8b2818a736f235d17
kapil212a <amangupta74086@gmail.com>
2026-06-13 14:52:51 +0530

    chanage

--- Week5.java
+++ Week5.java
@@ -89,6 +89,33 @@ public class Week5 {
         return ans;
     }
 
+    ///////// ADD BINARY \\\\\\\\\\\\\
+
+    public static String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+            if(i >= 0){
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0){
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            ans.append(sum % 2);
+            carry = sum /2;
+        }
+        return ans.reverse().toString();
+    }
+
+
 
     public static void main(String[] args) {
         int n = 3;
@@ -100,5 +127,10 @@ public class Week5 {
 
         int digits[] = {4,3,2,1};
         printArray(plusOne(digits));
+
+        String a = "1010";
+        String b = "1011";
+
+        System.out.println(addBinary(a, b));
     }
 }
