e2b2515c560eeae9a690cefc3543078e267959f6
kapil212a <amangupta74086@gmail.com>
2026-06-14 13:40:00 +0530

    chanage

--- Week5.java
+++ Week5.java
@@ -115,6 +115,30 @@ public class Week5 {
         return ans.reverse().toString();
     }
 
+    ///////////// CLACULATE SQURE ROOT VALUE \\\\\\\\\\\\\\\\
+
+    public static int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            long sqr = (long) mid * mid;
+            if(sqr == x){
+                return mid;
+            }
+
+            if(sqr < x){
+                ans = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
 
 
     public static void main(String[] args) {
