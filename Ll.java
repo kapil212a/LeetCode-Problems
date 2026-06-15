4ef3e8816e7dc2457315f1f988581879e36bee73
kapil212a <amangupta74086@gmail.com>
2026-06-15 14:32:59 +0530

    chanage

--- Ll.java
+++ Ll.java
@@ -214,6 +214,24 @@ public class Ll {
         return sum;
     }
 
+    //////////// Delete the Middle Node of a Linked List \\\\\\\\\\\\\
+
+    public static Node deleteMiddle(Node head){
+        if(head.next == null){
+             return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        return head;
+    }
+
 
     public static void main(String[] args) {
         Node head = new Node(1);
