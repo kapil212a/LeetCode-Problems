4457d698701a021dcfc39e309e33e8fe803ae02a
kapil212a <amangupta74086@gmail.com>
2026-06-17 13:36:55 +0530

    chanage

--- Street1.java
+++ Street1.java
@@ -189,6 +189,65 @@ public class Street1 {
         return res.toString();
     }
 
+    ////////////// Process String with Special Operations II \\\\\\\\\\\\\
+
+    public static char processStr(String s, long k) {
+        int n = s.length();
+        long len[] = new long[n];
+        long curr = 0;
+
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z'){
+                curr++;
+            }
+            else if(ch == '*'){
+                if(curr > 0){
+                    curr--;
+                }
+            }
+            else if(ch == '#'){
+                curr *= 2;
+            }
+            else if(ch == '%'){
+
+            }
+            len[i] = curr;
+        }
+        if(k >= curr){
+            return '.';
+        }
+        for(int i = n-1; i >= 0; i--){
+            char ch = s.charAt(i);
+
+            long prev;
+            if(i == 0){
+                prev = 0;
+            }
+            else{
+                prev = len[i - 1];
+            }
+
+            if(ch >= 'a' && ch <= 'z'){
+                if(k == prev){
+                    return ch;
+                }
+            }
+            else if(ch == '#'){
+                if(k >= prev){
+                    k = k - prev;
+                }
+            }
+            else if(ch == '%'){
+                k = prev -1 -k;
+            }
+        }
+        return '.';
+    }
+
+
+
     ///////////////// Construct the Minimum Bitwise Array I \\\\\\\\\\\\\\
 
     public static int[] minBitwiseArray(List<Integer> nums) {
