15f2324b94e7302bb744a9f7500f2c14fbc7ed5a
kapil212a <amangupta74086@gmail.com>
2026-06-18 13:39:38 +0530

    change

--- Street2.java
+++ Street2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Street2 {
     //////////// Angle Between Hands Of Clock \\\\\\\\\\\\\\\\
     
@@ -10,10 +12,29 @@ public class Street2 {
         return Math.min(angle , 360 - angle);
     }
 
+    //////////////// Minimum Difference Between Highest and Lowest of K Scores \\\\\\\\\\\\\\\\
+    
+    public static int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i <= nums.length - k ; i++){
+            min = Math.min(min,nums[i + k - 1] - nums[i]);
+        }
+        return min;
+    }
+
+
+
     public static void main(String[] args) {
         int hour = 12;
         int minutes = 30;
         System.out.println(angleClock(hour, minutes));
+
+        int nums[] = {9,4,1,7};
+        int k = 2;
+        System.out.println(minimumDifference(nums, k));
     }
 
 }
