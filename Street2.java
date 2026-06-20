e0554fb98cb5f3e0f2445a5d05c398026dcefe6f
kapil212a <amangupta74086@gmail.com>
2026-06-20 13:24:07 +0530

    add

--- Street2.java
+++ Street2.java
@@ -193,6 +193,13 @@ public class Street2 {
                           {1,3,1,5}};
         setZeroes(matrix);
         pritn2dMtrix(matrix);
+
+        int [][] matrix1 = {{1,3,5,7},
+                            {10,11,16,20},
+                            {23,30,34,60},
+                        };
+        int targte = 3;
+        
     }
 
 }
