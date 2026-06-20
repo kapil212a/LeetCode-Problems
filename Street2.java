e4153ceeb618e0d7e6cee3464959935408822cae
kapil212a <amangupta74086@gmail.com>
2026-06-20 13:22:11 +0530

    add

--- Street2.java
+++ Street2.java
@@ -137,6 +137,36 @@ public class Street2 {
         }
     }
 
+    /////////////// Search In 2D Matrix \\\\\\\\\\\\\\\\
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            int value = matrix[row][col];
+
+            if(value == target){
+                return true;
+            }
+            if(value < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
 
 
     public static void main(String[] args) {
