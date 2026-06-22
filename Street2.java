55c546cda6ef4058b19fef1c41d53c507f24cf71
kapil212a <amangupta74086@gmail.com>
2026-06-22 13:50:26 +0530

    add

--- Street2.java
+++ Street2.java
@@ -166,6 +166,32 @@ public class Street2 {
         }
         return false;
     }
+ ////////////////// Maximum Word Of Balloons In given Text \\\\\\\\\\\\\\\\\\\
+
+     public static int maxNumberOfBalloons(String text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+        for(char ch : text.toCharArray()){
+            if(ch == 'b'){
+                b++;
+            }
+            else if(ch == 'a'){
+                a++;
+            }
+            else if(ch == 'l'){
+                l++;
+            }
+            else if(ch == 'o'){
+                o++;
+            }
+            else if(ch == 'n'){
+                n++;
+            }
+        }
+        l = l / 2;
+        o = o / 2;
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o , n))));
+    }
 
 
 
