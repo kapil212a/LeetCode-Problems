b2e7364eb4091ad1c368a3c91841af1065683649
kapil212a <amangupta74086@gmail.com>
2026-06-23 14:28:10 +0530

    add

--- Week6.java
+++ Week6.java
@@ -116,6 +116,42 @@ public class Week6 {
         return j;
     }
 
+    //////////////////// Search In sorted Array \\\\\\\\\\\\\\\\\\\\\\\\\
+
+    public static boolean search(int[] nums, int target) {
+        int left = 0; 
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[left] == nums[mid]){
+                left++;
+            }
+            else if(nums[left] < nums[mid]){
+                if(target >= nums[left] && nums[mid] > target){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+
 
     public static void main(String[] args) {
         int nums[] = {2,0,2,1,1,0};
