d65682faec625226b39934882bd2acff8303eb53
kapil212a <amangupta74086@gmail.com>
2026-06-24 18:02:21 +0530

    add

--- Week6.java
+++ Week6.java
@@ -174,6 +174,22 @@ public class Week6 {
 
     }
 
+    ///////////////// Grey Code \\\\\\\\\\\\\\\\\\\\\
+
+    public static List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
+        for(int i = 0; i < n; i++){
+            int size = result.size();
+
+            int value = 1 << i;
+            for(int j = size - 1; j >= 0; j--){
+                result.add(result.get(j) + value);
+            }
+        }
+        return result;
+    }
 
 
 
