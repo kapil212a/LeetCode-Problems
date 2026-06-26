54587c595b834dad56b200efeaf044420b0c8c81
kapil212a <amangupta74086@gmail.com>
2026-06-26 16:36:02 +0530

    add

--- Tree.java
+++ Tree.java
@@ -26,6 +26,39 @@ public class Tree {
         inorder(node.right, ans);
     } 
 
+    ///////////////////// Build All Unique Binary Tree Possible \\\\\\\\\\\\\\\\\\
+
+    public static List<TreeNode> generateTrees(int n) {
+        if(n == 0){
+            return new ArrayList<>();
+        }
+        return buildBST(1 , n);
+    }
+    private static List<TreeNode> buildBST(int start , int end){
+        List<TreeNode> list  = new ArrayList<>();
+        if(start > end){
+            list.add(null);
+            return list;
+        }
+        for(int i = start; i <= end; i++){
+            List<TreeNode> left = buildBST(start , i - 1);
+            List<TreeNode> right = buildBST(i + 1 , end);
+
+            for(TreeNode l : left){
+               for(TreeNode r : right){
+                    TreeNode root  = new TreeNode(i);
+
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+
+
+
     public static void main(String[] args) {
         TreeNode node = new TreeNode(1);
         //node.left = new TreeNode(null);
@@ -33,5 +66,26 @@ public class Tree {
         node.right.left = new TreeNode(3);
         List<Integer> x = inorderTraversal(node);
         System.out.println(x);
+
+        int n = 3;
+        List<TreeNode> y = generateTrees(n);
+        //System.out.println(generateTrees(n));
+        for(TreeNode a : y){
+            System.out.print("[");
+            System.out.print(a.val + " ");
+           if(a.left == null){
+            System.out.print("null" + " ");
+           }
+           else{
+            System.out.print(a.left.val + " ");
+           }
+           if(a.right ==  null){
+            System.out.print("null" + " ");
+           }
+           else{
+            System.out.print(a.right.val + " ");
+           }
+           System.out.println("]");
+        }
     }
 }
