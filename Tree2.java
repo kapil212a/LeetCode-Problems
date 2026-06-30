dda60bf999d8c5920448c6150d80e67e38670669
kapil212a <amangupta74086@gmail.com>
2026-06-30 17:29:23 +0530

    add

--- Tree2.java
+++ Tree2.java
@@ -42,6 +42,26 @@ public class Tree2 {
         }
         return ans;
     }
+
+    //////////// Convert Sorted Array to Binary Search Tree \\\\\\\\\\\\
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums , 0 , nums.length - 1);
+    }
+
+    private static TreeNode buildTree(int nums[] , int left , int right){
+        if(left > right){
+            return null;
+        }
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildTree(nums, left,mid - 1);
+        root.right = buildTree(nums,mid + 1, right);
+
+        return root;
+    }
+
+
     public static void main(String[] args) {
 
         TreeNode node = new TreeNode(3);
@@ -51,6 +71,9 @@ public class Tree2 {
         node.right.left = new TreeNode(15);
         List<List<Integer>> x = levelOrderBottom(node);
         System.out.println(x);
+
+        int nums[] = {-10,-3,0,5,9};
+        System.out.println(sortedArrayToBST(nums));
 
     }
 
