017578511c0bd017dab1892eb32b12d47ed235da
kapil212a <amangupta74086@gmail.com>
2026-07-03 23:14:11 +0530

    add

--- Week7.java
+++ Week7.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Week7 {
     public static int numDistinct(String s, String t) {
         Integer [][] dp = new Integer [t.length()][s.length()];
@@ -23,6 +26,32 @@ public class Week7 {
         }
         return dp[i][j] = solve(t, s, i, j - 1,dp);
     }
+
+    /////////////////Pascal's Triangle\\\\\\\\\\
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<>();
+
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    row.add(1);
+                }
+                else{
+                    int left = ans.get(i - 1).get(j - 1);
+                    int right = ans.get(i - 1).get(j);
+
+                    row.add(left + right);
+                }
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+
+
 
     public static void main(String[] args) {
        String s = "rabbbit", t = "rabbit";
