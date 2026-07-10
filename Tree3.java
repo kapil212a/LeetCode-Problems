d820ed9918961636c99eb2ab4f44ebcf67daa03b
kapil212a <amangupta74086@gmail.com>
2026-07-10 22:38:38 +0530

    add

--- Tree3.java
+++ Tree3.java
@@ -26,4 +26,13 @@ public class Tree3 {
         }
         return dfs(root.left , curr) + dfs(root.right, curr);
     }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);           
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(sumNumbers(root));
+    }
 }
