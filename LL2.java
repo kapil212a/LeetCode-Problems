2bd9faeff7b243fbc279862cc3b03c132abbbbb5
kapil212a <amangupta74086@gmail.com>
2026-07-13 16:32:25 +0530

    add

--- LL2.java
+++ LL2.java
@@ -1,12 +1,21 @@
+import javax.swing.tree.TreeNode;
+
 public class LL2 {
     public static class ListNode{
         ListNode next;
-        int data;
+        int val;
 
-        public  ListNode(int data){
-            this.data = data;
+        public  ListNode(int val){
+            this.val = val;
             this.next = null;
         }
+    }
+    public static void printll(ListNode head){
+        ListNode temp = head;
+        while(temp != null){
+            System.out.print(temp.val + "-->");
+            temp = temp.next;
+        }System.out.println("null");
     }
 
     ////////////////// Detect Cycle in LinkedList\\\\\\\\\\\\\\\\
@@ -25,6 +34,25 @@ public class LL2 {
         return false;
     } 
 
+    //////////////////////// Insertion Sort in Tree \\\\\\\\\\\\\\\\\\\\\\\\
+
+    public static ListNode insertion(ListNode head){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            ListNode prev = dummy;
+
+            while(prev.next != null && prev.next.val < curr.val){
+                prev = prev.next;
+            }
+            curr.next = prev.next;
+            prev.next = curr;
+            curr = next;
+        }
+        return dummy.next;
+    }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
@@ -34,5 +62,15 @@ public class LL2 {
         head.next.next.next.next = new ListNode(3);
         head.next.next.next.next.next = new ListNode(4);
         System.out.println(hasCycle(head));
+
+        ListNode head1 = new ListNode(-1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(5);
+        head1.next.next.next = new ListNode(3);
+        head1.next.next.next.next = new ListNode(4);
+        head1.next.next.next.next.next = new ListNode(0); 
+        insertion(head1);
+        printll(head1);
+
     }
 }
