f32a9079e2ae1525df8ad5538737a9d62466a5e3
kapil212a <amangupta74086@gmail.com>
2026-07-16 14:00:46 +0530

    add

--- Week2.java
+++ Week2.java
@@ -75,7 +75,7 @@ public class Week2 {
             char ch = first.charAt(i);
 
             for(int j = 1; j<strs.length; j++){
-                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+                if(strs[j].charAt(i) != ch){
                     return first.substring(0,i);
                 }
             }
