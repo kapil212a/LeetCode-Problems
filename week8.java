5656ead81f7218bf8d21e2424d22b5226d4bcea9
kapil212a <amangupta74086@gmail.com>
2026-07-21 10:45:28 +0530

    add

--- week8.java
+++ week8.java
@@ -224,6 +224,23 @@ public class week8 {
         return dp[n];
     }
 
+    //////////////////FInd the largets element in O(logn) \\\\\\
+
+    public static int findPeakElement(int[] nums) {
+        int left = 0 , right = nums.length - 1;
+
+        while(left < right){
+            int mid = left + (right - left) /2;
+
+            if(nums[mid] < nums[mid + 1]){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return left;
+    }
     public static void main(String[] args) {
         int num[] = {100,4,200,1,3,2};
         System.out.println(longestConsecutive(num));
@@ -255,6 +272,9 @@ public class week8 {
         wordDict.add("code");
 
         System.out.println(wordBreak(s1, wordDict));
+
+        int ele[] = {1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(ele));
         
     }
 }
