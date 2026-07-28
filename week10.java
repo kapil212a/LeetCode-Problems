60e21eb725dd9372076c8d99e2c398ccb64f74b6
kapil212a <amangupta74086@gmail.com>
2026-07-28 17:15:14 +0530

    add

--- week10.java
+++ week10.java
@@ -10,8 +10,34 @@ public class week10 {
         return (int)sum;
     }
 
+    //////////////////////// Rotate The array from kth element \\\\\\\\\\\\\\\\\\\\\\\
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private static void reverse(int nums[] , int start , int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+
     public static void main(String[] args) {
         int n = 3;
         System.out.println(concatenatedBinary(n));
+
+        int arr[] = {-1,-100,3,99};
+        int k = 2;
+        rotate(arr, k);
+        
     }
 }
