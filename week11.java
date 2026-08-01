5d4aee6f062477d1a63c062497bfaec9d9650ff7
kapil212a <amangupta74086@gmail.com>
2026-08-01 18:40:17 +0530

    add

--- week11.java
+++ week11.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class week11 {
+    ////////////  The k-th Lexicographical String of All Happy Strings of Length n \\\\\\\\\\\\\\
     
+    static List<String> list = new ArrayList<>();
+    public static String getHappyString(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        backTrack(n , sb);
+        if(k > list.size()){
+            return "";
+        }
+
+        return list.get(k - 1);
+
+
+    }
+    private static void backTrack(int n, StringBuilder sb){
+        if(sb.length() == n){
+            list.add(sb.toString());
+            return;
+        }
+
+        char chStr[] = {'a', 'b', 'c'};
+
+        for(char ch : chStr){
+            if(sb.length() == 0 || sb.charAt(sb.length() - 1) != ch){
+                sb.append(ch);
+
+                backTrack(n, sb);
+
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 1 , k = 3;
+        System.out.println(getHappyString(n, k));
+    }
 }
