6f596dfc4a4ac1de5b19785fe80d22030d56fcb0
kapil212a <amangupta74086@gmail.com>
2026-08-04 19:51:10 +0530

    ad

--- week11.java
+++ week11.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -142,7 +143,38 @@ public class week11 {
         return ans;
     }
 
+/////////////////////////// Isomorphic String \\\\\\\\\\\\\\\\\\\\\\\
 
+public static boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            
+            if(map1.containsKey(ch1)){
+                if(map1.get(ch1) != ch2){
+                    return false;
+                }
+            }
+            else{
+                map1.put(ch1, ch2);
+            }
+            if(map2.containsKey(ch2)){
+                if(map2.get(ch2) != ch1){
+                    return false;
+                }
+            }
+            else{
+                map2.put(ch2, ch1);
+            }
+        }
+        
+        return true;
+    }
+
+    
     public static void main(String[] args) {
         int n = 1 , k = 3;
         System.out.println(getHappyString(n, k));
@@ -164,5 +196,8 @@ public class week11 {
         int num[] = {1,2,5};
         List<Integer> ans =  findMissingElements(num);
         System.out.println(ans);
+
+        String s = "paper", t = "title";
+        System.out.println(isIsomorphic(s,t));  
     }
 }
