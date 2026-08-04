569ba447b822da7c86bc769e1641b6b92bb0dc6a
kapil212a <amangupta74086@gmail.com>
2026-08-04 13:28:10 +0530

    add

--- week11.java
+++ week11.java
@@ -160,5 +160,9 @@ public class week11 {
 
         int number = 10;
         System.out.println(countPrimes(number));
+
+        int num[] = {1,4,2,5};
+        List<Integer> ans =  findMissingElements(num);
+        System.out.println(ans);
     }
 }
