f4139918b0e9ed3733a2b4cfa8f6a309025004ff
kapil212a <amangupta74086@gmail.com>
2026-08-06 15:34:08 +0530

    ad

--- week11.java
+++ week11.java
@@ -222,5 +222,6 @@ public static boolean isIsomorphic(String s, String t) {
         System.out.println(isIsomorphic(s,t));  
 
         int arr[] = {2,3,1,2,4,3};
+        System.out.println(minSubArrayLen(number, num));
     }
 }
