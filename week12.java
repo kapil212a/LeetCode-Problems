2771767bdd5d53898bd6aeaf065a34453bf01656
kapil212a <amangupta74086@gmail.com>
2026-08-09 19:50:24 +0530

    ch

--- week12.java
+++ week12.java
@@ -18,4 +18,11 @@ public class week12 {
 
 
     }
+
+
+    public static void main(String[] args) {
+       int nums[] = {3,2,1,5,6,4};
+       int k = 2;
+       System.out.println(findKthLargest(nums, k));
+    }
 }
