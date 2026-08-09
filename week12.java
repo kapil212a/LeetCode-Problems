f0df0a8bc841fef3e52ea879706611e6e1b2e603
kapil212a <amangupta74086@gmail.com>
2026-08-09 22:09:32 +0530

    ch

--- week12.java
+++ week12.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class week12 {
@@ -16,13 +18,42 @@ public class week12 {
         }
         return pq.peek();
 
-
     }
+
+    ///////////////// Combination Sum III \\\\\\\\\\\\\\\\
+
+    public static List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        //List<Integer> sum = new ArrayList<>();
+
+        solve(1, k, n, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private static void solve(int start, int k, int target, List<Integer> sum, List<List<Integer>>ans){
+        if(sum.size() == k){
+            if(target == 0){
+                ans.add(new ArrayList(sum));
+            }
+            return;    
+        }
+
+        for(int i = start; i <= 9; i++){
+            sum.add(i);
+            solve(i + 1, k, target - i, sum, ans);
+            sum.remove(sum.size() - 1);
+        }
+    }
+
 
 
     public static void main(String[] args) {
        int nums[] = {3,2,1,5,6,4};
        int k = 2;
        System.out.println(findKthLargest(nums, k));
+
+       int k1 = 3, n = 7;
+       List<List<Integer>> ans = combinationSum3(k1, n);
+       System.out.println(ans);
     }
 }
