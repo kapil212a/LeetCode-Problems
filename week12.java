f47fdfdafc7f7d0240f7c2df1e6e00d8dfea4bde
kapil212a <amangupta74086@gmail.com>
2026-08-13 00:11:11 +0530

    cha

--- week12.java
+++ week12.java
@@ -136,6 +136,33 @@ public class week12 {
         }
     }
 
+    ////////////////// Summary Range \\\\\\\\\\\\\\\\\\
+
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+
+        if(nums.length == 0){
+            return ans;
+        }
+
+        int start = nums[0];
+        for(int i = 1; i <= nums.length; i++){
+            if(i == nums.length || nums[i] != nums[i - 1] + 1){
+                if(start == nums[i - 1]){
+                    ans.add(String.valueOf(start));
+                }
+                else{
+                    ans.add(start + "->" + nums[i - 1]);
+                }
+                 if(i < nums.length){
+                    start = nums[i];
+                }
+            }
+           
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
        int nums[] = {3,2,1,5,6,4};
@@ -162,6 +189,10 @@ public class week12 {
        int nums1[] = {0,1,0,3,12};
        moveZeroes(nums1);
        printarr(nums1);
+
+       int nums2[] = {0,1,2,4,5,7};
+       List<String> ans1 = summaryRanges(nums2);
+       System.out.println(ans1);
 
     }
 }
