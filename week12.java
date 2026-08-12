5b23336820ed1b5f6446487a48052dfb423f3beb
kapil212a <amangupta74086@gmail.com>
2026-08-12 23:32:32 +0530

    cha

--- week12.java
+++ week12.java
@@ -116,6 +116,20 @@ public class week12 {
         return ans;
     }
 
+    //////////////////// Move Zeros TO end \\\\\\\\\\\\\\\\\\\
+
+    public static void moveZeroes(int[] nums) {
+        int j  = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
        int nums[] = {3,2,1,5,6,4};
