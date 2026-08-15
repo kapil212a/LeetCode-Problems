4a253eaa0eec4a72ce54f9336ce60a7c30d12dc1
kapil212a <amangupta74086@gmail.com>
2026-08-15 23:16:41 +0530

    add

--- week13.java
+++ week13.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class week13 {
     ///////Count Submatrices with Top-Left Element and Sum Less Than k\\\\\\\
     
@@ -27,6 +29,52 @@ public class week13 {
         }
         return ans;
     }
+
+    //////////////////// Basic Calculater \\\\\\\\\\\\\\\\\\\
+
+     public static int calculate(String s) {
+        int result = 0;
+        int num = 0;
+        int sign = 1;
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(Character.isDigit(ch)){
+                num = num * 10 + (ch - '0');
+            }
+            else if(ch == '+'){
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if(ch == '-'){
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if(ch == '('){
+                st.push(result);
+                st.push(sign);
+                
+                result = 0;
+                sign = 1;
+            }
+            else if(ch == ')'){
+                result += sign * num;
+                num = 0;
+
+                int prevSign = st.pop();
+                int prevResult = st.pop();
+
+                result = prevResult + prevSign * result;
+            }
+        }
+        result += sign * num;
+        return result;
+    }
+
 
 
     public static void main(String[] args) {
