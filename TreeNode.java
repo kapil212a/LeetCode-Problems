35d71bb7589cc50400a5a86cc3cf715507db38c0
kapil212a <amangupta74086@gmail.com>
2026-08-19 20:17:12 +0530

    add

--- TreeNode.java
+++ TreeNode.java
@@ -67,6 +67,22 @@ class BSTIterator {
         return count;
     }
 
+    /////////////////// Invert TreeNode \\\\\\\\\\\\\\\\\\\\\
+
+    public static TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(7);
