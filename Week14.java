e2ea5484acb91673feeefc04aa8c223dc1a4f87b
kapil212a <amangupta74086@gmail.com>
2026-09-03 19:14:01 +0530

    ad

--- Week14.java
+++ Week14.java
@@ -103,6 +103,28 @@ public class Week14 {
         return true;
     }
 
+    /////////////////// Search In 2D matrix \\\\\\\\\\\\\\\\\\\\
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m - 1;
+
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+
 
 
     public static void main(String[] args) {
@@ -117,6 +139,14 @@ public class Week14 {
 
         int arr3[] = {1,4,7};
         System.out.println(uniformArray(arr3));
+
+        int matrix[][] = {{1,4,7,11,15},
+                          {2,5,8,12,19},
+                          {3,6,9,16,22},
+                          {10,13,14,17,24},
+                          {18,21,23,26,30}
+        };
+        System.out.println(searchMatrix(matrix, 19));
 
 
     }
