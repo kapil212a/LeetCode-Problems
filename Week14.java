d6d3f41825d641f7abc6e96a2f894ceedaa6f56b
kapil212a <amangupta74086@gmail.com>
2026-09-03 13:14:44 +0530

    add

--- Week14.java
+++ Week14.java
@@ -85,6 +85,25 @@ public class Week14 {
         return ans;
     }
 
+    ////////////////// Construct Uniform Parity Array II \\\\\\\\\\\\\\\
+
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+        for(int x : nums1){
+            min = Math.min(min, x);
+        }
+        if(min % 2 == 1){
+            return true;
+        }
+        for(int x : nums1){
+            if(x % 2 == 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
 
     public static void main(String[] args) {
         String num = "5023"; ////////// ?3295???;
