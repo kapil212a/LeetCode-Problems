bcc3d79b2b2aef688b72aaf16683a8cf6067b66d
kapil212a <amangupta74086@gmail.com>
2026-09-05 09:59:35 +0530

    ad

--- Week14.java
+++ Week14.java
@@ -190,6 +190,26 @@ public class Week14 {
         return -1;
 
     }
+
+    //////////////// Find Ugly Number \\\\\\\\\\\\\\\\\\\\\
+    
+    public boolean isUgly(int n) {
+        if(n <= 0){
+            return false;
+        }
+        while(n % 2 == 0){
+            n /= 2;
+        }
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        while(n % 5 == 0){
+            n /= 5;
+        }
+        return n == 1;
+    }
+
+
     public static void main(String[] args) {
         String num = "5023"; ////////// ?3295???;
         System.err.println(sumGame(num));
@@ -218,6 +238,8 @@ public class Week14 {
         int arr4[] = {5,0,1,4};
         int k = 3;
         System.out.println(firstStableIndex(arr4, k));
+
+        
     }
 
 }
