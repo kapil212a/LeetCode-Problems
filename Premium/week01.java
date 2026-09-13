1a83181ce823e6135025c0d0028fb772e6195954
kapil212a <amangupta74086@gmail.com>
2026-09-13 13:25:24 +0530

    add

--- Premium/week01.java
+++ Premium/week01.java
@@ -1,5 +1,34 @@
 package Premium;
 
+import java.util.HashMap;
+
 public class week01 {
-    
+    ////////////longest Substring with at most two  distinct character \\\\\\\\\\\\
+    public static int distinct(String s){
+        int max = 0;
+        int left = 0;
+        HashMap <Character , Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while(map.size() > 2){
+                char newCh = s.charAt(left);
+                map.put(newCh, map.get(newCh) - 1);
+
+                if(map.get(newCh) == 0){
+                    map.remove(newCh);
+                }
+                left++;
+            }
+            max = Math.max(max, i - left + 1);
+           
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        String s = "eceba";
+        System.out.println(distinct(s));
+    }
 }
